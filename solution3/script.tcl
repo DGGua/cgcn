3e5c972a76538c6a2bb9c8d9e3ba44b2242dea78
DGGua <41000029+DGGua@users.noreply.github.com>
2022-11-05 15:53:45 +0800

    buf

--- solution3/script.tcl
+++ solution3/script.tcl
@@ -9,8 +9,9 @@ add_files cgcn/src/systolic.cpp
 add_files cgcn/src/test.cpp
 add_files -tb cgcn/src/testbench.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
 open_solution "solution3" -flow_target vivado
-set_part {xc7z010-clg400-2}
+set_part {xc7z045ffv900-1}
 create_clock -period 10 -name default
+config_interface -m_axi_max_widen_bitwidth 512
 source "./cgcn/solution3/directives.tcl"
 csim_design -clean -profile
 csynth_design
--- src/systolic.cpp
+++ src/systolic.cpp
@@ -7,7 +7,7 @@ void PE(hls::stream<compute_type>& property_input,
         hls::stream<compute_type>& data_output,
         int turn) {
   compute_type a = 0, b = 0;
-  compute_type sum[8] = {0, 0, 0, 0,0, 0, 0, 0};
+  compute_type sum[8] = {0, 0, 0, 0, 0, 0, 0, 0};
   ap_uint<3> flag = 0;
 PE_Compute:
   for (int i = 0; i < turn; i++) {
@@ -16,7 +16,8 @@ PE_Compute:
     sum[flag] += a * b;
     flag += 1;
   }
-  data_output.write(((sum[7] + sum[6]) + (sum[5] + sum[4]))+((sum[3] + sum[2]) + (sum[1] + sum[0])));
+  data_output.write(((sum[7] + sum[6]) + (sum[5] + sum[4])) +
+                    ((sum[3] + sum[2]) + (sum[1] + sum[0])));
 }
 
 void input_property(int batch,
@@ -25,13 +26,14 @@ void input_property(int batch,
                         property_input[ARRAY_HEIGHT][MAX_OUTPUT]) {
 input_turn_property:
   for (int turn = 0; turn < FEATURE_LENGTH; turn++) {
+    compute_type property_val[ARRAY_HEIGHT];
+    memcpy(property_val, featrue_data + turn * ARRAY_HEIGHT,
+           sizeof(compute_type) * ARRAY_HEIGHT);
   input_property_row:
     for (int row = 0; row < ARRAY_HEIGHT; row++) {
-      compute_type featrue_val =
-          featrue_data[(batch * ARRAY_HEIGHT + row) * FEATURE_LENGTH + turn];
     input_property_col:
       for (int col = 0; col < MAX_OUTPUT; col++) {
-        property_input[row][col].write(featrue_val);
+        property_input[row][col].write(property_val[row]);
       }
     }
   }
@@ -44,12 +46,14 @@ void input_weight(int batch,
                       weight_input[ARRAY_HEIGHT][MAX_OUTPUT]) {
 input_turn_weight:
   for (int turn = 0; turn < FEATURE_LENGTH; turn++) {
+    compute_type weight_val[MAX_OUTPUT];
+    memcpy(weight_val, weight_array + turn * MAX_OUTPUT,
+           sizeof(compute_type) * MAX_OUTPUT);
   input_weight_col:
     for (int col = 0; col < MAX_OUTPUT; col++) {
-      compute_type weight_val = weight_array[turn * output_size + col];
     input_weight_row:
       for (int row = 0; row < ARRAY_HEIGHT; row++) {
-        weight_input[row][col].write(weight_val);
+        weight_input[row][col].write(weight_val[col]);
       }
     }
   }
@@ -93,11 +97,15 @@ void rerArray(compute_type* featrue_data,
   hls::stream<compute_type, MAX_OUTPUT> weight_input[ARRAY_HEIGHT][MAX_OUTPUT];
   hls::stream<compute_type, 1> output_stream[ARRAY_HEIGHT][MAX_OUTPUT];
   const unsigned batchnum = node_cnt / ARRAY_HEIGHT;
-  int buff[ARRAY_HEIGHT][MAX_OUTPUT];
+  compute_type weight_buff[MAX_OUTPUT * FEATURE_LENGTH];
+  rerArray_label2:for (int i = 0; i < MAX_OUTPUT * FEATURE_LENGTH; i++) {
+    weight_buff[i] = weight_array[i];
+  }
+  compute_type buff[ARRAY_HEIGHT][MAX_OUTPUT];
 batch_round:
   for (int batch = 0; batch < batchnum; batch++) {
     input_property(batch, featrue_data, property_input);
-    input_weight(batch, weight_array, output_size, weight_input);
+    input_weight(batch, weight_buff, output_size, weight_input);
     PE_compute(property_input, weight_input, output_stream);
     output(output_data, output_size, output_stream);
   }
