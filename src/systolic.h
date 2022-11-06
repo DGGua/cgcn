0743dafdc382b154a0be17b590f3ee8d101db8de
DGGua <41000029+DGGua@users.noreply.github.com>
2022-11-06 15:51:58 +0800

    update header

--- src/systolic.h
+++ src/systolic.h
@@ -1,5 +1,31 @@
+#include <ap_fixed.h>
 #include <hls_stream.h>
+#include <string.h>
 #include "defines.h"
+
+void PE(hls::stream<compute_type>& property_input,
+        hls::stream<compute_type>& weight_input,
+        hls::stream<compute_type>& data_output,
+        int turn);
+void input_property(int batch,
+                    compute_type* featrue_data,
+                    hls::stream<compute_type, MAX_INPUT>
+                        property_input[ARRAY_HEIGHT][MAX_OUTPUT]);
+
+void input_weight(int batch,
+                  compute_type* weight_array,
+                  int output_size,
+                  hls::stream<compute_type, MAX_INPUT>
+                      weight_input[ARRAY_HEIGHT][MAX_OUTPUT]);
+void output(
+    compute_type* output_data,
+    int output_size,
+    hls::stream<compute_type, 1> output_stream[ARRAY_HEIGHT][MAX_OUTPUT]);
+void PE_compute(hls::stream<compute_type, MAX_INPUT>
+                    property_input[ARRAY_HEIGHT][MAX_OUTPUT],
+                hls::stream<compute_type, MAX_OUTPUT> weight_input[ARRAY_HEIGHT]
+                                                                  [MAX_OUTPUT],
+                hls::stream<compute_type, 1> output[ARRAY_HEIGHT][MAX_OUTPUT]);
 void rerArray(compute_type* featrue_data,
               int featrue_length,
               compute_type* weight_array,
